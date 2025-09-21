c660a349ce73445dfe71467d6d31a54650d0992c
NIKSON.D <niksonlovely7@gmail.com>
2025-09-21 22:21:56 +0530

    while loop.java

---
+++ while loop.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class cond1
+{
+	public static void main(String[] args) {
+	    Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int i=n;
+        while(i>=1)
+        {        System.out.print(i+" ");
+                    i--;  
+                    System.out.println("A");
+        }
+    }
+}
