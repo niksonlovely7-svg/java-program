259f7fa2b9f42219cb0fd2f7b9d0ea5513a0512b
NIKSON.D <niksonlovely7@gmail.com>
2025-09-21 22:20:55 +0530

    leap year.java

---
+++ leap year.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class cond1
+{
+	public static void main(String[] args) {
+	    Scanner kbd=new Scanner(System.in);
+	    int year=kbd.nextInt();
+        if(year%400==0 || year%100!=0&&year%4==0)
+        System.out.println("leap year");
+        else
+        System.out.println("non leap year");
+	}
+}
